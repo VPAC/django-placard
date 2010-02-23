61ab37741a5dfef50876e4f7e473a749983ecb12
Sam Morrison <sam@vpac.org>
2010-02-23 11:47:04 +1100

    Test settings changes

--- demo/settings.py
+++ demo/settings.py
@@ -80,5 +80,5 @@ LDAP_GROUP_BASE='ou=Group, %s' % LDAP_BASE
 LDAP_ATTRS = 'demo.ldap_attrs'
 
 
-TEST_RUNNER='demo.test_utils.xmlrunner.run_tests'
+TEST_RUNNER='test_utils.xmlrunner.run_tests'
 DATABASE_ENGINE = 'sqlite3'
