e72df6d7936f3ca1729b55b2d63a0d25290de7c4
Sam Morrison <sam@vpac.org>
2010-03-01 16:46:58 +1100

    Added GPLv3 to all source files

--- placard/backends.py
+++ placard/backends.py
@@ -1,3 +1,21 @@
+# Copyright 2010 VPAC
+#
+# This file is part of django-placard.
+#
+# django-placard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django-placard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django-placard  If not, see <http://www.gnu.org/licenses/>.
+
+
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
