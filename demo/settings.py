55d324ce166fbafa5d419edb383ef12aae25cbb5
Sam Morrison <sam@vpac.org>
2011-02-14 10:49:48 +1100

    Add django admin to fix test errors

--- demo/settings.py
+++ demo/settings.py
@@ -3,6 +3,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.admin',
     'placard',
     'placard.lusers',
     'placard.lgroups',
