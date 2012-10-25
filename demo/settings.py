VPAC/django-placard

commit d9a6456e343ddcdfdafdb9a2bca08c5fd9693170

Update settings.

demo/settings.py
@@ -31,10 +31,17 @@ DATABASES = {
     }
 }
 
-LDAP_USE_TLS=False
-LDAP_URL = 'ldap://localhost:38911'
+LDAP = {
+    'default': {
+        'ENGINE': 'tldap.backend.transaction',
+        'URI': 'ldap://sys12.in.vpac.org/',
+        'USER': 'cn=admin,dc=nodomain',
+        'PASSWORD': 'j1pUXrz2Qj',
+        'USE_TLS': False,
+        'TLS_CA' : None,
+    }
+}
 
-LDAP_ADMIN_PASSWORD="password"
 LDAP_BASE="dc=python-ldap,dc=org"
 LDAP_ADMIN_USER="cn=Manager,dc=python-ldap,dc=org"
 LDAP_USER_BASE='ou=People, %s' % LDAP_BASE

commit 094f507dbc94876b5b5c1a386949d7fb6149a88c

Broke the settings in d9a6456e343ddcdfdafdb9a2bca08c5fd9693170, oops.

demo/settings.py
@@ -34,9 +34,9 @@ DATABASES = {
 LDAP = {
     'default': {
         'ENGINE': 'tldap.backend.transaction',
-        'URI': 'ldap://sys12.in.vpac.org/',
-        'USER': 'cn=admin,dc=nodomain',
-        'PASSWORD': 'j1pUXrz2Qj',
+        'URI': 'ldap://localhost:38911/',
+        'USER': 'cn=Manager,dc=python-ldap,dc=org',
+        'PASSWORD': 'password',
         'USE_TLS': False,
         'TLS_CA' : None,
     }