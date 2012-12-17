ede8820af0dbd25bd901a9808cb7a06c7719b198
Brian May <brian@microcomaustralia.com.au>
2012-12-17 11:33:30 +1100

    Initialize counters using existing ldap data.

---
+++ placard/migrations/0002_set_counters.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+import placard.ldap_bonds
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        if db.dry_run:
+            return
+
+        master = placard.ldap_bonds.master
+
+        last_uid = None
+        for obj in master.accounts():
+            if last_uid is None or obj.uidNumber > last_uid:
+                last_uid = obj.uidNumber
+        if last_uid is not None:
+            entry, created = orm.counters.objects.get_or_create(name="uidNumber", defaults = { 'count': last_uid + 1 })
+            if not created:
+                entry.count = last_uid + 1
+                entry.save()
+
+        last_gid = None
+        for obj in master.groups():
+            if last_gid is None or obj.gidNumber > last_gid:
+                last_gid = obj.gidNumber
+        if last_gid is not None:
+            entry, created = orm.counters.objects.get_or_create(name="gidNumber", defaults = { 'count': last_gid + 1 })
+            if not created:
+                entry.count = last_gid + 1
+                entry.save()
+
+    def backwards(self, orm):
+        pass
+
+
+    models = {
+        'placard.counters': {
+            'Meta': {'object_name': 'counters'},
+            'count': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['placard']
